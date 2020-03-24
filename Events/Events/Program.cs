4c8ff4353164feb01061691f7c159fdff6ea9963
Flip182 <360flip182@gmail.com>
2020-03-24 15:16:25 +0700

    Event homework

---
+++ Events/Events/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Event MyEvent = new Event();
+            MyEvent.TimeEvent += counter.Handler1;
+
+            MyEvent.OnTimeEvent();
+        }
+    }
+
+    delegate void CounterHandler();
+
+    class Event
+    {
+        public event CounterHandler TimeEvent;
+
+        public void OnTimeEvent()
+        {
+            TimeEvent();
+        }
+    }
+
+    public class Counter
+    {
+        public void Handler1()
+        {
+            Random rnd = new Random();
+            int stopPoint = rnd.Next(0, 100);
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == stopPoint)
+                {
+
+                    Console.WriteLine($"Пора действовать, ведь уже {stopPoint}!");
+                }
+            }
+        }
+    }
+}
